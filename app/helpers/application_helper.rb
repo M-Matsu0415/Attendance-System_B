897f085d4c65cf2154c0920cbac422da696517f7
M-Matsu0415 <matsu0322@jcom.zaq.ne.jp>
2020-09-08 17:03:05 +0000

    4.4.4メールアドレスの検証性から

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   
-  def full_title(page_name = "")
+  def full_title(page_name = "") #ページ名称を文字列で指定
     base_title = "AttendanceApp"
     if page_name.empty?
       base_title
