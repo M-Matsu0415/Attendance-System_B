ee14508c90431472f27ddd2899e6730a7086cb95
M-Matsu0415 <matsu0322@jcom.zaq.ne.jp>
2020-09-14 09:32:18 +0000

    10.6 勤怠登録ボタン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,33 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper  # sessionsヘルパーが使用できるように宣言
+  
+  $days_of_the_week = %w{日 月 火 水 木 金 土}
+  # $にてグローバル変数を定義。リテラル表記にて配列["日", "月", "火", "水", "木", "金", "土"]と同等。
+  
+  # ページ出力前に1ヶ月分のデータの存在を確認・セットします。
+  def set_one_month
+    @first_day = params[:date].nil? ?
+    Date.current.beginning_of_month : params[:date].to_date
+    # to_date：Time型からDate型に変換できる。
+    @last_day = @first_day.end_of_month
+    one_month = [*@first_day..@last_day] # 対象の月の日数を代入します。
+    # ユーザーに紐付く一ヶ月分のレコードを検索し取得します。
+    @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
+    
+    unless one_month.count == @attendances.count # それぞれの件数（日数）が一致するか評価します。
+      ActiveRecord::Base.transaction do # トランザクションを開始します。
+        # 繰り返し処理により、1ヶ月分の勤怠データを生成します。
+        # worked_onに日付の入ったAttendanceモデルのデータが生成される。
+        # create!：最後に!を付けることで保存できない場合例外ActiveRecord::RecordInvalidが発生する。
+        # トランザクションは例外が発生した場合にロールバックされる。
+        one_month.each { |day| @user.attendances.create!(worked_on: day) }
+      end
+      @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
+    end
+    
+  rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
+    flash[:danger] = "ページ情報の取得に失敗しました、再アクセスしてください。"
+    redirect_to root_url
+  end
 end
