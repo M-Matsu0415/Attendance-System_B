2cb984933edc85b9d0c351e2eeb10208f496a3e3
M-Matsu0415 <matsu0322@jcom.zaq.ne.jp>
2020-09-15 15:00:10 +0000

    10章 直し完了

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -9,4 +9,11 @@ module AttendancesHelper
     # どれにも当てはまらなかった場合はfalseを返します。
     false
   end
+  
+  # 出勤時間と退勤時間を受け取り、在社時間を計算して返します。
+  def working_times(start, finish)
+    format("%.2f", (((finish - start) / 60) / 60.0))
+    # %は数値によって変化し、.2fは値がない場合は.00、値がある場合はそのまま、小数点第三位以上まである場合は第二位まで表示しそれ以降は切り捨てます。
+    # formatメソッドの基本形　format("%.f", 数値)：数値をfで指示された桁数に揃えて返す-->
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
   def format_basic_info(time)
     format("%.2f", ((time.hour * 60) + time.min) / 60.0)
     # %は数値によって変化し、.2fは値がない場合は.00、値がある場合はそのまま、小数点第三位以上まである場合は第二位まで表示しそれ以降は切り捨てます。
-    # formatメソッドの基本形　format("%.f", 数値)：数値をfで指示された桁数に揃えて返す-->
+    # formatメソッドの基本形　format("%.f", 数値)：数値をfで指示された桁数に揃えて文字列で返す-->
   end
   
 end
