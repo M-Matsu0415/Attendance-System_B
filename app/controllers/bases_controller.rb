131e41c7d781aee0c6c4ff7156130e0dd37df35c
M-Matsu0415 <matsu0322@jcom.zaq.ne.jp>
2020-10-08 16:26:54 +0000

    拠点情報バリデーション実装中

--- app/controllers/bases_controller.rb
+++ app/controllers/bases_controller.rb
@@ -1,4 +1,48 @@
 class BasesController < ApplicationController
+
   def new
+    @base = Base.new
   end
+  
+  def create
+    @base = Base.new(base_params)
+    if @base.save
+      flash[:success] = "拠点追加に成功しました"
+      redirect_to bases_path
+    else
+      render  :new
+    end
+  end
+  
+  def index
+    @bases = Base.all
+  end
+  
+  def destroy
+    @base = Base.find(params[:id])
+    @base.destroy
+    flash[:success] = "#{@base.name}のデータを削除しました。"
+    redirect_to bases_url
+  end
+  
+  def edit
+    @base = Base.find(params[:id])
+  end
+  
+  def update
+    @base = Base.find(params[:id])
+    if @base.update_attributes(base_params)
+      flash[:success] = "拠点情報を更新しました"
+      redirect_to bases_url
+    else
+      render :edit
+    end
+  end
+  
+  private
+
+    def base_params
+      params.require(:base).permit(:name, :number, :status)
+    end
+
 end
--- app/models/base.rb
+++ app/models/base.rb
@@ -1,2 +1,4 @@
 class Base < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 10 }
+  validates :number, presence: true, length: { maximum: 5 }
 end
