f9750dc415750ef473df0babd19d706229110cc3
M-Matsu0415 <matsu0322@jcom.zaq.ne.jp>
2020-10-23 15:15:42 +0000

    csv出力機能完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,8 +66,32 @@ class UsersController < ApplicationController
   
   def csv_output
     respond_to do |format|
-      format.csv { send_data @attendances.generate_csv, filename: "勤怠情報.csv" }
+      format.csv do |csv|
+        send_attendances_csv(@attendances)
+      end
     end
+  end
+  
+  def send_attendances_csv(attendances)
+    csv_data = CSV.generate(encoding: Encoding::SJIS) do |csv|
+      
+      column_names = %w(日付 出勤時間 退勤時間)
+      csv << column_names
+      
+      attendances.each do |attendance|
+        if !attendance.started_at.nil?
+          started_at_csv = attendance.started_at.strftime("%H：%M")
+        end
+        
+        if !attendance.finished_at.nil?
+          finished_at_csv = attendance.finished_at.strftime("%H：%M")
+        end
+        
+        column_values = [ attendance.worked_on, started_at_csv, finished_at_csv ]
+        csv << column_values
+      end
+    end
+    send_data(csv_data, filename: "勤怠情報.csv")
   end
 
   def update_basic_info
