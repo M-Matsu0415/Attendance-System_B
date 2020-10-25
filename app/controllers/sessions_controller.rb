b72f669af99332aef06ea6f186fe00107b53090f
M-Matsu0415 <matsu0322@jcom.zaq.ne.jp>
2020-10-25 17:18:36 +0000

    一ヶ月分の承認実装中(途中なのでshowにてエラー出る状態)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,18 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      # 有効なオブジェクトが取得できている(user：左辺がnilでない)、且つ　authenticateメソッドにより
+      # パスワードの認証に成功した場合(右辺がtrue)の場合にif文の条件分岐が成立する。
       log_in user
       params[:session][:remember_me] == '1'? remember(user) : forget(user)
       flash[:success] = "ログインしました。"
-      redirect_back_or user
+      if user.admin?
+        redirect_back_or root_url
+      else
+        redirect_back_or user
+      # redirect_back_orの引数にuserを指定することでデフォルトのurlを指定している。
+      # userは、user_urlの略、ルーティングはusers#showなので、showアクションが働き、勤怠情報画面に遷移する)
+      end
     else
       flash.now[:danger] = "認証に失敗しました。"
       render  :new
