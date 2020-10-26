24f8f9edb5837442ed949cdd0a37d71fd68c6acd
M-Matsu0415 <matsu0322@jcom.zaq.ne.jp>
2020-10-26 17:37:59 +0000

    一ヶ月の勤怠承認実装中

--- app/controllers/month_apprpvals_controller.rb
+++ app/controllers/month_apprpvals_controller.rb
@@ -1,4 +1,25 @@
 class MonthApprpvalsController < ApplicationController
+  before_action :set_user, only: :create
+  before_action :set_one_month, only: :create
+  
   def new
   end
+  
+  def create
+    @month_approval = month_approval.new(month_approval_params)
+    @month_approval.applicant_user_id = @user.id
+    @month_approval.approval_status = 1
+    if @month_approval.save
+      flash[:success] = "に成功しました"
+      redirect_to @user
+    else
+      render  :new
+    end
+  end
+  
+  private
+    def month_approval_params
+      params.require(:month_approval).permit(:approval_superior_id)
+    end
+  
 end
